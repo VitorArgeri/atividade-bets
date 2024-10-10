dd930cfc2b4e3dd1a312d9819c8a0410a6419cb3
VitorArgeri <vitor.argeri@aluno.senai.br>
2024-10-10 14:33:09 -0300

    feat: add get method

--- src/routes/suspeitos.js
+++ src/routes/suspeitos.js
@@ -5,3 +5,9 @@ const suspeitosRoutes = Router()
 let suspeitos = [
     
 ]
+
+// Rota para buscar todos os candidatos
+suspeitosRoutes.get("/", (req, res) => {
+    return res.status(200)
+    .send( suspeitos ) 
+})
