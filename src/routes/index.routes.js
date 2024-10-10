6758ef1d3ddda53481a55b37229455e67b1a7317
VitorArgeri <vitor.argeri@aluno.senai.br>
2024-10-10 14:49:20 -0300

    feat: index.routes.js

---
+++ src/routes/index.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import candidatosRoutes from "./candidatos.routes.js";
+
+const routes = Router();
+
+routes.get("/", (req, res) => {
+    return res.status(200).send({
+        message: "Vamo São paulo!!!"
+    })
+})
+
+routes.use('/candidatos', candidatosRoutes);
+
+export default routes
--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
-import express from "express";
-import { config } from "dotenv";
+import express from 'express'
+import { config } from 'dotenv'
 
-config();
+config()
+import routes from "./routes/index.routes.js";
 
-const serverPort = process.env.PORT || 4000;
+const serverPort = process.env.PORT || 4000
 
 const app = express();
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
+app.use(routes);
 
 app.listen(serverPort, () => {
-  console.log(`⚡ Server started on http://localhost:${serverPort}`);
-});
+    console.log(`😎 Server iniciado em http://localhost:${serverPort}`)
+})
